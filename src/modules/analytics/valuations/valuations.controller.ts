84d99bea57e02a35b086cae3945c069a2d2229c2
QabbasKhan <qabbas65khan@gmail.com>
2025-06-09 00:18:38 -0700

    ref: changes in valuations: with refinance calc

--- src/modules/analytics/valuations/valuations.controller.ts
+++ src/modules/analytics/valuations/valuations.controller.ts
@@ -1,48 +1,19 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ValuationsService } from './valuations.service';
-import { CreateValuationDto } from './dto/create-valuation.dto';
-import { UpdateValuationDto } from './dto/update-valuation.dto';
-import { CreateAnalyticsDto } from '../dto/create-analytics.dto';
 
 @Controller({ path: 'valuations', version: '1' })
 export class ValuationsController {
   constructor(private readonly valuationsService: ValuationsService) {}
 
-  @Post('/test')
-  async getValuation(@Body() dto: CreateValuationDto) {
-    const data = await this.valuationsService.calculateValuation(dto);
-    return { data };
-  }
+  // @Post('/test')
+  // async getValuation(@Body() dto: CreateValuationDto) {
+  //   const data = await this.valuationsService.calculateValuation(dto);
+  //   return { data };
+  // }
 
   @Get('/:id')
   async findOne(@Param('id') id: string) {
     const data = await this.valuationsService.calculateValuationFromId(id);
     return { data };
   }
-
-  // @Get()
-  // findAll() {
-  //   return this.valuationsService.findAll();
-  // }
-
-  // @Patch(':id')
-  // update(
-  //   @Param('id') id: string,
-  //   @Body() updateValuationDto: UpdateValuationDto,
-  // ) {
-  //   return this.valuationsService.update(+id, updateValuationDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.valuationsService.remove(+id);
-  // }
 }
